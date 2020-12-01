e82fbd687c90e5a77e6ebc87d2cd040702c07a2c
****** ****** <bogdan@s-MacBook-Pro.local>
2020-12-01 16:39:09 +0200

    Adapted collectionView
    Adapted collectionView.

--- LoveStor/Controller/Registration/AvatarSelectionViewController.swift
+++ LoveStor/Controller/Registration/AvatarSelectionViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 class AvatarSelectionViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
-    @IBOutlet weak var collectionViewHeightConstraint: NSLayoutConstraint!
     
     let iconsArray = [UIImage(named: "avatar1"), UIImage(named: "avatar2"), UIImage(named: "avatar3"), UIImage(named: "avatar4"), UIImage(named: "avatar5"), UIImage(named: "avatar6"), UIImage(named: "avatar7"), UIImage(named: "avatar8"), UIImage(named: "avatar9"), UIImage(named: "avatar10"), UIImage(named: "avatar11"), UIImage(named: "avatar12")]
     let iconsNamesArray = ["avatar1", "avatar2", "avatar3", "avatar4", "avatar5", "avatar6",  "avatar7", "avatar8", "avatar9", "avatar10", "avatar11", "avatar12"]
@@ -20,24 +19,7 @@ class AvatarSelectionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setupCollectionView()
-    }
-    
-    func setupCollectionView() {
         collectionView.register(UINib(nibName: "ProfileIconCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "profileIconCell")
-        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-        if UIScreen.main.bounds.height < 668.0 {
-            collectionViewHeightConstraint.constant = 439.0
-            let cellSize = (collectionView.frame.width/3)-25
-            layout.minimumInteritemSpacing = 1
-            layout.minimumLineSpacing = 15
-            layout.itemSize = CGSize(width: cellSize, height: cellSize)
-        } else {
-            layout.minimumInteritemSpacing = 20
-            layout.minimumLineSpacing = 20
-            layout.itemSize = CGSize(width: 110, height: 110)
-        }
-        collectionView.collectionViewLayout = layout\
     }
     
     @IBAction func saveButtonTapped(_ sender: Any) {
@@ -46,7 +28,7 @@ class AvatarSelectionViewController: UIViewController {
     }
 }
 
-extension AvatarSelectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+extension AvatarSelectionViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return iconsArray.count
     }
@@ -59,5 +41,12 @@ extension AvatarSelectionViewController: UICollectionViewDelegate, UICollectionV
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         selectedIndex = indexPath.row
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let collectionViewWidth = collectionView.frame.width
+        let sideSize = (collectionViewWidth/3) - 13
+        let cellSize = CGSize(width: sideSize, height: sideSize)
+        return cellSize
     }
 }
